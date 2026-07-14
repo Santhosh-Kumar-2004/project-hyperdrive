c528b3d301cde56d30fd7f2780013a49ee6276c8
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-07-14 21:32:39 +0530

    Created the integrer and also the boolean data type variables

--- js_fundamentals.js
+++ js_fundamentals.js
@@ -2,3 +2,9 @@ const firstname = "Santhosh"
 const lastname = "Kumar"
 
 console.log(firstname + " " + lastname)
+
+const age_of_san = 21
+const isDev = false
+
+console.log(age_of_san)
+console.log(isDev)
