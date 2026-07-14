a8ee224a184f34a3dabfb8da1ed90b51a1089e7a
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-07-14 21:36:08 +0530

    Created the big console log statement to print all of things at once

--- js_fundamentals.js
+++ js_fundamentals.js
@@ -4,7 +4,9 @@ const lastname = "Kumar"
 console.log(firstname + " " + lastname)
 
 const age_of_san = 21
-const isDev = false
+const isDev = true
 
 console.log(age_of_san)
 console.log(isDev)
+
+console.log(firstname + " " + lastname + "'s age is" + " " + age_of_san + " " + "and he's dev? = " + isDev)
