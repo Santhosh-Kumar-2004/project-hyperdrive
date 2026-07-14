8f38ddeabfbccd5f59904813e53c8547f9d72288
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-07-14 21:37:01 +0530

    Trying out the var

--- js_fundamentals.js
+++ js_fundamentals.js
@@ -10,3 +10,7 @@ console.log(age_of_san)
 console.log(isDev)
 
 console.log(firstname + " " + lastname + "'s age is" + " " + age_of_san + " " + "and he's dev? = " + isDev)
+
+var name = "santhosh"
+
+console.log(name)
